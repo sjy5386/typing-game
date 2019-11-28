d187e44922933f5d41d2524e24cd247079342714
Seokjin Yoon <sjy5386@gmail.com>
2019-11-28 13:24:37 +0900

    Update WordLabel

--- src/view/WordLabel.java
+++ src/view/WordLabel.java
@@ -6,17 +6,19 @@ import java.util.Random;
 
 public class WordLabel extends JLabel {
     private static final int MIN_X = 0;
-    private static final int MAX_X = 600;
+    private static final int MAX_X = 800;
     private static final int MIN_Y= 0;
-    private static final int MAX_Y = 400;
+    private static final int MAX_Y = 600;
 
     public WordLabel(String text) {
         super(text);
+        setFont(new Font("맑은 고딕", Font.BOLD, 16));
+        int width = getFont().getSize() * text.length();
+        int height = getFont().getSize() * 2;
+        setSize(width, height);
         Random random = new Random();
-        int x = random.nextInt(MAX_X + 1) + MIN_X;
-        int y = random.nextInt(MAX_Y + 1) + MIN_Y;
+        int x = random.nextInt(MAX_X + 1 - width) + MIN_X;
+        int y = random.nextInt(MAX_Y + 1 - height) + MIN_Y;
         setLocation(x, y);
-        setSize(text.length() * 16, 32);
-        setFont(new Font("맑은 고딕", Font.PLAIN, 16));
     }
 }
