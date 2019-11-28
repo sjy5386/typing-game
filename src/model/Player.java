a5f5da9ba5c86eec0d1cb40a54149415e1bf30e9
Seokjin Yoon <sjy5386@gmail.com>
2019-11-28 13:00:58 +0900

    Add Player

---
+++ src/model/Player.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Player {
+    private String name = "익명";
+    private int score = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
