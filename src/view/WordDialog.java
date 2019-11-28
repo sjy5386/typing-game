343b41778637e222c2cbc50854ba776b26f57726
Seokjin Yoon <sjy5386@gmail.com>
2019-11-29 00:21:20 +0900

    Update WordDialog

--- src/view/WordDialog.java
+++ src/view/WordDialog.java
@@ -4,11 +4,12 @@ import javax.swing.*;
 import java.awt.*;
 
 public class WordDialog extends JDialog {
+    private WordPanel wordPanel = new WordPanel();
+
     public WordDialog(Frame owner) {
-        super(owner);
-        setTitle("단어");
+        super(owner, "단어", true);
         setMinimumSize(new Dimension(400, 430));
         setResizable(false);
-        add(new WordPanel());
+        setContentPane(wordPanel);
     }
 }
