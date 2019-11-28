ebab7c01deba4f8527e9a150b7a6cf3603a2839b
Seokjin Yoon <sjy5386@gmail.com>
2019-11-28 19:50:37 +0900

    Add LeaderBoardManager

---
+++ src/model/LeaderBoardManager.java
@@ -0,0 +1,68 @@
+package model;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
+
+public class LeaderBoardManager {
+    private static Vector<Player> leaderBoard = new Vector<>();
+    private static final String filename = "dat/leaderboard.txt";
+
+    public LeaderBoardManager() {
+        if (leaderBoard.size() == 0)
+            load();
+    }
+
+    public Vector<Player> getLeaderBoard() {
+        return leaderBoard;
+    }
+
+    public void add(Player player) {
+        leaderBoard.add(player);
+        Collections.sort(leaderBoard);
+    }
+
+    public void reset() {
+        leaderBoard.removeAllElements();
+    }
+
+    public boolean save() {
+        try {
+            FileWriter fout = new FileWriter(filename, StandardCharsets.UTF_8);
+            BufferedWriter out = new BufferedWriter(fout);
+            for (Player line:leaderBoard)
+                out.write(line.getName() + "|" + line.getScore() + "\n");
+            out.close();
+            fout.close();
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    public boolean load() {
+        if (Files.exists(Paths.get(filename)))
+            return false;
+
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(filename), StandardCharsets.UTF_8);
+            Vector<Player> v = new Vector<>();
+            for (String line:lines) {
+                String name = line.split("|")[0].trim();
+                int score = Integer.parseInt(line.split("|")[1]);
+                v.add(new Player(name, score));
+            }
+            Collections.sort(v);
+            leaderBoard = v;
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+}
--- src/model/Player.java
+++ src/model/Player.java
@@ -1,15 +1,30 @@
 package model;
 
-public class Player {
-    private String name = "익명";
-    private int score = 0;
+public class Player implements Comparable<Player> {
+    private String name;
+    private int score;
+
+    public Player() {
+        name = "익명";
+        score = 0;
+    }
+
+    public Player(String name) {
+        this.name = name.trim();
+        score = 0;
+    }
+
+    public Player(String name, int score) {
+        this.name = name.trim();
+        this.score = score;
+    }
 
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name = name.trim();
     }
 
     public int getScore() {
@@ -18,5 +33,10 @@ public class Player {
 
     public void setScore(int score) {
         this.score = score;
+    }
+
+    @Override
+    public int compareTo(Player o) {
+        return o.getScore() - this.score;
     }
 }
