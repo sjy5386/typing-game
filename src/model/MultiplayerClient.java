a20ebd684f05be5d828ef04521b187fd2584ba20
Seokjin Yoon <sjy5386@gmail.com>
2019-12-13 16:11:33 +0900

    Update MVC

--- src/model/MultiplayerClient.java
+++ src/model/MultiplayerClient.java
@@ -1,3 +1,5 @@
+package model;
+
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
--- src/model/MultiplayerServer.java
+++ src/model/MultiplayerServer.java
@@ -1,3 +1,5 @@
+package model;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.util.Scanner;
